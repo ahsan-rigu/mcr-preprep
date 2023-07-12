ace0cd6d618a374072940704c081729bc1f639ef
ahsan-rigu <114474110+ahsan-rigu@users.noreply.github.com>
2023-07-12 21:40:39 +0530

    feat: css, header with darkmode button, cra done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import logo from "./logo.svg";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Header from "./components/Header";
+import Home from "./pages/Home";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Header.css";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header>
+      <Link to="/" className="logo">
+        MCR 8 | Ahsan
+      </Link>
+      <nav></nav>
+      <label class="switch">
+        <input type="checkbox" className="mode-switch" />
+        <span class="slider"></span>
+      </label>
+    </header>
+  );
+};
+
+export default Header;
